429a59654f7e7aaff7b302a18f021c9474d0e12c
Unknown <jeremy.coulombe@hotmail.fr>
2017-04-01 20:12:24 -0400

    commencer graphique
    f

--- devoir2/src/AffichageLaby.java
+++ devoir2/src/AffichageLaby.java
@@ -1,5 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by jeremycoulombe on 17-04-01.
  */
-public class AffichageLaby {
+public class AffichageLaby extends JComponent
+{
+    Labyrinthe labyrinthe;
+
+    public AffichageLaby(Labyrinthe labyrinthe)
+    {
+        this.labyrinthe=labyrinthe;
+    }
+
+    public void paintComponent (Graphics g)
+    {
+        super.paintComponent(g);//comme dans l'exemple en clasee
+
+        Graphics2D graphics2D=(Graphics2D)g;//cast pour avoir plus de methodes
+
+        int largeur = getWidth();//obtient largeur disponible
+
+        int hauteur=getHeight();//obtient hauteur disponible
+
+        graphics2D.drawRect(0,0,largeur,hauteur);//met rectangle pour enlever ce qui etait la avant
+        //coordonne a verifier
+
+    }
+
 }
--- devoir2/src/JPanelLaby.java
+++ devoir2/src/JPanelLaby.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by jeremycoulombe on 17-04-01.
  */
-public class JPanelLaby {
+public class JPanelLaby extends JPanel
+{
+    public JPanelLaby(int largeur,int hauteur,double densiteLaby,double delai, int nbVie)
+    {
+        AffichageLaby affichageLaby= new AffichageLaby(new Labyrinthe(largeur,hauteur,densiteLaby,delai,nbVie));
+        //cree affichageLaby qui sera afficher au Centre
+
+        setLayout(new BorderLayout());//mets le Borderlayout comme layout
+
+        add(affichageLaby,BorderLayout.CENTER);//mets affichageLaby au centre
+
+    }
 }
